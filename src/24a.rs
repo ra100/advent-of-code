bbaccca7a3d0c5d43240b8f07ac9cc69734d9673
🇺🇦 Svarba, Rastislav <rastislav.svarba@merck.com>
2025-01-02 10:07:02 +0100

    🚧 24 part 2

--- src/24a.rs
+++ src/24a.rs
@@ -8,8 +8,8 @@ fn main() {
     let input = fs::read_to_string("src/24.txt").expect("Failed to read file");
 
     let mut initial_wires: HashMap<&str, u8> = HashMap::new();
-    let mut initial_gates: Vec<(&str, &str, &str, &str)> = Vec::new();
-    let mut max_bit_index: usize = 0;
+    let mut initial_gates: HashMap<&str, (&str, &str, &str)> = HashMap::new();
+    let mut max_bit_length: usize = 0;
 
     for line in input.trim().lines() {
         let line = line.trim();
@@ -18,39 +18,44 @@ fn main() {
             initial_wires.insert(parts[0], parts[1].parse().unwrap());
         } else if line.contains("->") {
             let parts: Vec<&str> = line.split(" ").collect();
-            initial_gates.push((parts[0], parts[1], parts[2], parts[4]));
+            initial_gates.insert(parts[4], (parts[0], parts[1], parts[2]));
             if parts[4].starts_with("z") {
                 let number: Vec<&str> = parts[4].split("z").collect();
                 let number: usize = number[1].parse().unwrap();
-                max_bit_index = max_bit_index.max(number);
+                max_bit_length = max_bit_length.max(number);
             }
         }
     }
+    let max_x_length: usize = initial_wires.len() / 2;
+    max_bit_length += 1;
+    println!("Bit count: {}", max_bit_length);
 
-    max_bit_index -= 2;
-    println!("Bit count: {}", max_bit_index);
+    let static_gates = initial_gates.clone();
 
     // Expected output for addition
-    let x_bits: Vec<u8> = (0..max_bit_index)
+    let x_bits: Vec<u8> = (0..max_x_length)
         .map(|i| *initial_wires.get(&format!("x{:02}", i)[..]).unwrap())
+        .rev()
         .collect();
-    let y_bits: Vec<u8> = (0..max_bit_index)
+    let y_bits: Vec<u8> = (0..max_x_length)
         .map(|i| *initial_wires.get(&format!("y{:02}", i)[..]).unwrap())
+        .rev()
         .collect();
-    println!("x: {:?}", x_bits);
-    println!("y: {:?}", y_bits);
     let x_value = bits_to_number(&x_bits);
     let y_value = bits_to_number(&y_bits);
     let expected_sum = x_value + y_value;
-    let expected_bits = number_to_bits(expected_sum, max_bit_index);
+    let expected_bits = number_to_bits(expected_sum, max_bit_length);
+    println!("x: {:?}", x_bits);
+    println!("y: {:?}", y_bits);
+    println!("z: {:?}", expected_bits);
 
     println!("Expected sum: {}", expected_sum);
 
     let start = Instant::now();
     let discrepancies = find_discrepancies(
         &initial_wires,
-        &initial_gates,
-        max_bit_index,
+        &static_gates,
+        max_bit_length,
         &expected_bits,
     );
     let x_discrepancies = discrepancies
@@ -67,6 +72,7 @@ fn main() {
         .collect::<Vec<_>>();
     let duration = start.elapsed();
     println!("Time taken: {:?}", duration);
+    println!("Discrepancies: {:?}", discrepancies);
 
     // find invalid gates
     let mut invalid_gates_forward = Vec::new();
@@ -74,29 +80,27 @@ fn main() {
     let mut wires_to_follow = VecDeque::new();
     let mut z_wires_to_follow = VecDeque::new();
 
-    for &(input1, gate, input2, output) in &initial_gates {
+    for (&output, &(input1, _, input2)) in static_gates.iter() {
         if y_discrepancies.contains(&input1.to_string())
             || y_discrepancies.contains(&input2.to_string())
             || x_discrepancies.contains(&input1.to_string())
             || x_discrepancies.contains(&input2.to_string())
         {
-            invalid_gates_forward.push((input1, gate, input2, output));
+            invalid_gates_forward.push(output);
             wires_to_follow.push_back(output);
         }
 
         if z_discrepancies.contains(&output.to_string()) {
-            invalid_gates.push((input1, gate, input2, output));
+            invalid_gates.push(output);
             z_wires_to_follow.push_back(input1);
             z_wires_to_follow.push_back(input2);
         }
     }
 
     while let Some(wire) = wires_to_follow.pop_front() {
-        for &(input1, gate, input2, output) in &initial_gates {
-            if (input1 == wire || input2 == wire)
-                && !invalid_gates_forward.contains(&(input1, gate, input2, output))
-            {
-                invalid_gates_forward.push((input1, gate, input2, output));
+        for (&output, &(input1, _, input2)) in static_gates.iter() {
+            if (input1 == wire || input2 == wire) && !invalid_gates_forward.contains(&output) {
+                invalid_gates_forward.push(&output);
                 wires_to_follow.push_back(output);
             }
         }
@@ -109,12 +113,12 @@ fn main() {
     // );
 
     while let Some(wire) = z_wires_to_follow.pop_front() {
-        for &(input1, gate, input2, output) in &initial_gates {
+        for (&output, &(input1, _, input2)) in static_gates.iter() {
             if output == wire
-                && !invalid_gates.contains(&(input1, gate, input2, output))
-                && invalid_gates_forward.contains(&(input1, gate, input2, output))
+                && !invalid_gates.contains(&output)
+                && invalid_gates_forward.contains(&output)
             {
-                invalid_gates.push((input1, gate, input2, output));
+                invalid_gates.push(output);
                 z_wires_to_follow.push_back(input1);
                 z_wires_to_follow.push_back(input2);
             }
@@ -123,79 +127,73 @@ fn main() {
 
     // println!("Invalid gates: {} {:?}", invalid_gates.len(), invalid_gates);
 
-    let mut swapped_wires = HashSet::new();
-    let mut useless_wires = HashSet::new();
-    let mut modified_gates = initial_gates.clone();
-    let possible_gates = invalid_gates.clone();
+    let mut tried_wires: HashSet<&str> = HashSet::new();
+    let mut possible_swaps: HashMap<(&str, &str), isize> = HashMap::new();
+    let modified_gates = static_gates.clone();
+    let mut possible_gates: Vec<&str> = invalid_gates.clone();
+    // let mut possible_gates: Vec<&str> = static_gates.keys().map(|&k| k).collect();
+    possible_gates.sort();
+    let discrepancies_length = discrepancies.len() as isize;
+    // let mut shortest = discrepancies.len();
 
-    for &invalid_gate in &possible_gates {
-        let mut found = false;
-        if useless_wires.contains(invalid_gate.3) {
+    for &out_wire in &possible_gates {
+        println!("Checking wire: {}", out_wire);
+        if tried_wires.contains(out_wire) {
             continue;
         }
-        for &gate_to_swap in &possible_gates {
-            if gate_to_swap == invalid_gate {
+        for &wire_to_swap in &possible_gates {
+            if wire_to_swap == out_wire || tried_wires.contains(wire_to_swap) {
                 continue;
             }
-            if useless_wires.contains(gate_to_swap.3) {
-                continue;
-            }
-            let mut new_gates: Vec<(&str, &str, &str, &str)> = modified_gates
-                .iter()
-                .filter(|&item| item.3 == invalid_gate.3 || item.3 == gate_to_swap.3)
-                .cloned()
-                .collect();
-            new_gates.push((
-                gate_to_swap.0,
-                gate_to_swap.1,
-                gate_to_swap.2,
-                invalid_gate.3,
-            ));
-            new_gates.push((
-                invalid_gate.0,
-                invalid_gate.1,
-                invalid_gate.2,
-                gate_to_swap.3,
-            ));
 
-            let new_discrepancies =
-                find_discrepancies(&initial_wires, &new_gates, max_bit_index, &expected_bits);
+            let mut temp_gates = modified_gates.clone();
+            let gate1 = *temp_gates.get(out_wire).unwrap();
+            let gate2 = *temp_gates.get(wire_to_swap).unwrap();
+            temp_gates.insert(wire_to_swap, gate1);
+            temp_gates.insert(out_wire, gate2);
 
-            if new_discrepancies.len() <= discrepancies.len() {
-                println!("Swapping {} and {}", invalid_gate.3, gate_to_swap.3);
-                println!(
-                    "New discrepancies: {} {}",
-                    new_discrepancies.len(),
-                    discrepancies.len()
+            let mod_discrepancies =
+                find_discrepancies(&initial_wires, &temp_gates, max_bit_length, &expected_bits);
+
+            if (mod_discrepancies.len() as isize) < discrepancies_length {
+                // println!("Swapped {} with {}", out_wire, wire_to_swap);
+                // println!("Discrepancies: {:?}", mod_discrepancies);
+                possible_swaps.insert(
+                    (out_wire, wire_to_swap),
+                    discrepancies_length - mod_discrepancies.len() as isize,
                 );
-                swapped_wires.insert(invalid_gate.3);
-                swapped_wires.insert(gate_to_swap.3);
-                modified_gates = new_gates;
-                found = true;
             }
         }
-        if !found {
-            useless_wires.insert(invalid_gate.3);
-        }
+        tried_wires.insert(out_wire);
     }
 
-    println!("Swapped wires: {:?}", swapped_wires);
+    println!("Possible swaps: {:?}", possible_swaps.len());
+
+    // let swapped_wires: HashSet<&str> = HashSet::new();
+
+    // let mut swapped_wires_vec: Vec<&str> = swapped_wires.into_iter().collect();
+    // swapped_wires_vec.sort();
+
+    // println!(
+    //     "Swapped wires: {} {:?}",
+    //     swapped_wires_vec.len(),
+    //     swapped_wires_vec.join(",")
+    // );
 }
 
 fn find_discrepancies(
     initial_wires: &HashMap<&str, u8>,
-    initial_gates: &Vec<(&str, &str, &str, &str)>,
+    initial_gates: &HashMap<&str, (&str, &str, &str)>,
     max_bit_index: usize,
     expected_bits: &Vec<u8>,
-) -> Vec<String> {
+) -> HashSet<String> {
     let mut wires = initial_wires.clone();
     let mut gates = initial_gates.clone();
     let mut processed = true;
     while processed {
         processed = false;
-        let mut remaining_gates = Vec::new();
-
-        for (input1, gate, input2, output) in gates {
+        let mut used_gates = Vec::new();
+        for (&output, &(input1, gate, input2)) in gates.iter() {
             if let (Some(&val1), Some(&val2)) = (wires.get(input1), wires.get(input2)) {
                 let result = match gate {
                     "AND" => val1 & val2,
@@ -204,27 +202,27 @@ fn find_discrepancies(
                     _ => panic!("Unknown gate"),
                 };
                 wires.insert(output, result);
+                used_gates.push(output);
                 processed = true;
-            } else {
-                remaining_gates.push((input1, gate, input2, output));
             }
         }
-
-        gates = remaining_gates;
+        for gate in used_gates {
+            gates.remove(gate);
+        }
     }
 
     // Compare actual output with expected output
-    let mut discrepancies = Vec::new();
+    let mut discrepancies: HashSet<String> = HashSet::new();
     for i in 0..max_bit_index {
         let wire = format!("z{:02}", i);
         if let Some(&actual_value) = wires.get(&wire[..]) {
             if actual_value != expected_bits[i] {
-                discrepancies.push(wire.split_at(1).1.to_string());
+                discrepancies.insert(wire.split_at(1).1.to_string());
             }
         }
     }
 
-    println!("Discrepancies: {:?}", discrepancies);
+    // println!("Discrepancies: {:?}", discrepancies);
     discrepancies
 }
 
